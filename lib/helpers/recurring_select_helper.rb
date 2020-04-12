b66123c79783cbf2e1c13d25152cc16a0ba1a9ab
Ross Philip <ross@helpatschools.com>
2020-04-12 09:05:09 -0600

    Merge changes from Syndicode:rails_6_support fork

--- lib/helpers/recurring_select_helper.rb
+++ lib/helpers/recurring_select_helper.rb
@@ -12,7 +12,7 @@ module RecurringSelectHelper
       end
     end
   end
-  
+
   module FormBuilder
     def select_recurring(method, default_schedules = nil, options = {}, html_options = {})
       if !@template.respond_to?(:select_recurring)
@@ -25,8 +25,6 @@ module RecurringSelectHelper
 
   module FormOptionsHelper
     def recurring_options_for_select(currently_selected_rule = nil, default_schedules = nil, options = {})
-#byebug
-      currently_selected_rule = nil if currently_selected_rule == "null"
 
       options_array = []
       blank_option_label = options[:blank_label] || I18n.t("recurring_select.not_recurring")
@@ -65,17 +63,6 @@ module RecurringSelectHelper
     end
 
     private
-
-#    def recurring=(value)
-#byebug
-#      if value == "null"
-#        super(nil)
-#      elsif RecurringSelect.is_valid_rule?(value)
-#        super(RecurringSelect.dirty_hash_to_rule(value).to_hash)
-#      else
-#        super(nil)
-#      end
-#    end
 
     def ice_cube_rule_to_option(supplied_rule, custom = false)
       return supplied_rule unless RecurringSelect.is_valid_rule?(supplied_rule)
--- lib/recurring_select/engine.rb
+++ lib/recurring_select/engine.rb
@@ -1,20 +1,19 @@
+require "helpers/recurring_select_helper"
+require "middleware/recurring_select_middleware"
+
 module RecurringSelect
   class Engine < Rails::Engine
-    
+
     initializer "recurring_select.extending_form_builder" do |app|
     # config.to_prepare do
       ActionView::Helpers::FormHelper.send(:include, RecurringSelectHelper::FormHelper)
       ActionView::Helpers::FormOptionsHelper.send(:include, RecurringSelectHelper::FormOptionsHelper)
       ActionView::Helpers::FormBuilder.send(:include, RecurringSelectHelper::FormBuilder)
     end
-    
+
     initializer "recurring_select.connecting_middleware" do |app|
-      if Rails::VERSION::STRING >= '5.2'
-        app.config.app_middleware.use RecurringSelectMiddleware
-      else
-        app.middleware.use RecurringSelectMiddleware # insert_after ActionDispatch::ParamsParser,
-      end
+      app.middleware.use RecurringSelectMiddleware # insert_after ActionDispatch::ParamsParser,
     end
-    
+
   end
 end
